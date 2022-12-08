fa34e899ecf5694e8761485b158e8b629ba652f8
Seralyn <seralyn84@gmail.com>
2022-12-08 15:34:23 -0500

    syncing before merge with weather-dashboard

--- src/components/WeatherInfoBlock.js
+++ src/components/WeatherInfoBlock.js
@@ -1,7 +1,16 @@
-function WeatherInfoBlock() {
+import axios from "axios";
+import { useState } from 'react';
+
+
+function WeatherInfoBlock(props) {
+
+    const APIurl = "https://my.meteoblue.com/packages/basic-1h_basic-day_current_clouds-1h_clouds-day_sunmoon?apikey=5JiOrKtqP7ngm0Ap&lat=54.6892&lon=25.2798&asl=98&format=json&winddirection=3char&forecast_days=5&history_days=2";
+
+
+
     return ( 
-        <div className="w-10 h-10">
-            <span className="text-3xl text-white">C</span>
+        <div className="mx-10">
+            <span className="text-3xl text-white">Category</span>
         </div>
      );
 }
